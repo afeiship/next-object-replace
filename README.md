9c044c50caed1aee239fae0481fc2197868c143c
afeiship <afeiship@gmail.com>
2019-01-27 15:48:38 +0800

    feat: init & teset

---
+++ README.md
@@ -0,0 +1,15 @@
+# next-object-replace
+> Replace for object key/value
+
+## install:
+```bash
+npm install -S afeiship/next-object-replace --registry=https://registry.npm.taobao.org
+```
+
+## usage:
+```js
+//DOCS here!
+```
+
+## related:
+- [next-array-replace](https://github.com/afeiship/next-array-replace)
---
+++ dist/next-object-replace.js
@@ -0,0 +1,16 @@
+(function() {
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+
+  nx.objectReplace = function(inString, inObject) {
+    if (!inString) return;
+    nx.forIn(inObject, function(key, value) {
+      inString = inString.replace(key, value);
+    });
+    return inString;
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.objectReplace;
+  }
+})();
